8a17d5a373a4aea654758ad00e38d154c1008429
Xiangfu <xiangfu@openmobilefree.net>
2012-10-12 09:47:39 +0800

    qi_lb60: add software usbboot support
      JZ4740 CPU have a internal ROM have such kind of code, that make
      JZ4740 can boot from USB
    
      usbboot.S can downloads user program from the USB port to internal
      SRAM and branches to the internal SRAM to execute the program
    
    Signed-off-by: Xiangfu <xiangfu@openmobilefree.net>

--- board/qi/qi_lb60/qi_lb60-spl.c
+++ board/qi/qi_lb60/qi_lb60-spl.c
@@ -12,6 +12,24 @@
 #include <asm/io.h>
 #include <asm/jz4740.h>
 
+#define KEY_U_OUT       (32 * 2 + 16)
+#define KEY_U_IN        (32 * 3 + 19)
+
+extern void usb_boot(void);
+
+static void check_usb_boot(void)
+{
+	__gpio_as_input(KEY_U_IN);
+	__gpio_enable_pull(KEY_U_IN);
+	__gpio_as_output(KEY_U_OUT);
+	__gpio_clear_pin(KEY_U_OUT);
+
+	if (!__gpio_get_pin(KEY_U_IN)) {
+		puts("[U] pressed, goto USBBOOT mode\n");
+		usb_boot();
+	}
+}
+
 void nand_spl_boot(void)
 {
 	__gpio_as_sdram_16bit_4720();
@@ -22,6 +40,8 @@ void nand_spl_boot(void)
 
 	pll_init();
 	sdram_init();
+
+	check_usb_boot();
 
 	nand_init();
 
